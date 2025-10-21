4d108b823ee48156c5ff3fa2d9dcc0a966f3460e
Sumanthvu <sumanthveslavath07@gmail.com>
2025-10-21 14:12:49 +0530

    add user model logic

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,52 @@
+import mongoose, { Schema, trusted } from "mongoose";
+
+const tripSchema = new mongoose.Schema(
+  {
+    tripPlan: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    emailVerificationOtp: {
+      type: String,
+    },
+    emailVerificationOtpExpires: {
+      type: Date,
+    },
+    passwordResetOtp: {
+      type: String,
+    },
+    passwordResetOtpExprires: {
+      type: Date,
+    },
+    trips: [tripSchema],
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
